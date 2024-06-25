b6e04ac9b9cc1acf5eb64f5f8d738631aa1907d7
samglover <sam@samglover.net>
2024-06-25 15:02:20 -0500

    Add archive banner

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,22 @@
-var msg = `The Lab is hosting a <a href="https://suffolklitlab.org/LITCon/2024/" target="_blank">hybrid event at Suffolk Law</a> on April 8th on AI and law practice. Join us!`
+var msg = `This website is an archive. <a href="https://suffolklitlab.org">Click here to visit the current Suffolk LIT Lab website.</a>`
 
-function close_msg() {
-    document.body.style["background-position"] = "top -65px center";
-    document.getElementById('msg_bar').style.display='none';
-    localStorage.setItem('msg',msg)
-}
+// function close_msg() {
+//     document.body.style["background-position"] = "top -65px center";
+//     document.getElementById('msg_bar').style.display='none';
+//     localStorage.setItem('msg',msg)
+// }
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    if (localStorage.msg != msg) {
-        document.getElementById('msg_bar').innerHTML = `<a href="javascript:close_msg();" class="ex">X</a>`+ msg;
-        document.getElementById('msg_bar').style.display='block';
-        document.getElementById('msg_bar').style.display='block';    
-        document.body.style["background-position"] = "top -24px center";
-    }
+    // if (localStorage.msg != msg) {
+    //     document.getElementById('msg_bar').innerHTML = `<a href="javascript:close_msg();" class="ex">X</a>`+ msg;
+    //     document.getElementById('msg_bar').style.display='block';
+    //     document.getElementById('msg_bar').style.display='block';    
+    //     document.body.style["background-position"] = "top -24px center";
+    // }
+
+    document.getElementById('msg_bar').innerHTML = msg;
+    document.getElementById('msg_bar').style.display='block';    
+    document.body.style["background-position"] = "top -24px center";
 
 });
